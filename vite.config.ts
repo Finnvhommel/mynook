37a1b84e22c7163f547bc2da730e9c6426f9a86a
Finnvhommel <finnvhommel@gmail.com>
2026-02-12 00:56:07 +0100

    hopelijk de 404 fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // VOEG DIT TOE: Vervang 'mynook' door de exacte naam van je GitHub repo
+  // Voor een eigen domein (mynookapp.com) gebruiken we altijd de root '/'
   base: '/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
